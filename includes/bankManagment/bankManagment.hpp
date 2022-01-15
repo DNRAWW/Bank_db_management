773df80ff937e0c5bf4d52dff46565d91a9839e0
Kostya.Moguchev <rjcnz222@gmail.com>
2022-01-15 16:51:47 +0300

    Provider header

--- includes/bankManagment/bankManagment.hpp
+++ includes/bankManagment/bankManagment.hpp
@@ -5,13 +5,15 @@
 
 class BankManagment {
     public: 
+        BankManagment(Provider* provider);
         void add();
-        void change(uint64_t id);
-
-
+        void deleteById(uint64_t id);
+        std::vector<Customer*> getAllCustomers();
+        Customer* getOneCustomer(uint64_t id);
 
     private:
-
+        Provider* provider;
+        Repository* repository;
 };
 
 #endif
